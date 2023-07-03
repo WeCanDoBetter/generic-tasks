be46ce5563a0dd060d2cb429eb545d9dd05e7019
Michiel van der Velde <michiel@michielvdvelde.nl>
2023-07-03 13:55:29 +0200

    chore: remove other code

--- src/lib/util..ts
+++
@@ -1,7 +0,0 @@
-import { createHash } from "crypto";
-
-export function hashFunctionContents(func: Function): string {
-  const functionString = func.toString();
-  const hash = createHash("sha256").update(functionString).digest("hex");
-  return hash;
-}
